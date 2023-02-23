1b9a5941cde7457e39bdda5703f9ac3c52b67bd3
Jaro05 <62530116+Jaro05@users.noreply.github.com>
2023-02-23 15:02:55 -0500

    adding some functions

--- parse.c
+++ parse.c
@@ -8,8 +8,10 @@ int main(int argc, char *argv[]){
     // 2) PART 1 (100 points).
     //    Generate an AST of the file by calling generateAST in the parseProgram.c file
     //    This is the meat and potatoes of the project, we need to write the generateAST function.
+    AST* progast = parseProgam();
 
     // 3) Call parser_close to close the file.
+    praser_close();
 
     // 4) Call the given unparse functions to actaully output the program to stdoutt. The unparse functions go
     //    through the AST and print it out in the correct format. We dont have to write these functions, they are given.
--- parseProgram.c
+++ parseProgram.c
@@ -21,10 +21,97 @@ void parser_close()
     lexer_close();
 }
 
-AST* generateAST(){
+void advance()
+{
+    if (!lexer_done()) {
+	    tok = lexer_next();
+    }
+}
+
+void eat(token_type tt) {
+    if (tok.typ == tt) {
+	// debug_print("Eating a %s token\n", ttyp2str(tt));
+        advance();
+    } else {
+        token_type expected[1] = {tt};
+        parse_error_unexpected(expected, 1, tok);
+    }
+}
+
+AST* parseProgram(){
     // Call lexer next and such to keep getting tokens. Slowly build the whole AST.
     // We will need to create a bunch of helper functions in this file, like parseIfStmt, parseWhileStmt, etc...
+    AST_list cds = parseConstDecls();
+    AST_list vds = parseVarDecls();
+    AST* stmt = parseStmt();
 
+    return(ast_program());
     
+}
+
+AST_list parseConstDecls(){
+
+}
+
+AST_list parseVarDecls(){
+    
+}
+
+AST* parseStmt(){
+    AST* ret = NULL;
+    switch(tok.typ){
+        case identsym:
+            ret = parseAssignStmt();
+            break;
+        case beginsym:
+            ret = parseBeginStmt();
+            break;
+        case ifsym:
+            ret = parseIfStmt();
+            break;
+        case whilesym:
+            ret = parseWhileStmt();
+            break;
+        case readsym:
+            ret = parseIfStmt();
+            break;
+        case writesym:
+            ret = parseWriteStmt();
+            break;
+        case skipsym:
+            ret = parseSkipStmt();
+            break;
+        default:
+	       // parse_error_unexpected(a, a, tok);
+    }
+
+    return(ret);
+}
+
+AST* parseIfStmt(){
+    token ift = tok;
+    eat(ifsym);
+    AST* cond = parseCondition();
+    eat(thensym);
+    AST* stmt1 = parseStmt();
+    eat(elsesym);
+    AST* stmt2 = parseStmt();
+
+    return(ast_if_stmt(/*stuff*/));
+}
+
+AST* parseCondition(){
+    token condt = tok;
+    if(condt.typ == oddsym){
+        eat(oddsym);
+        AST* e1 = parseExpr();
+        return(ast_odd_cond(tok, e1));
+    }else{
+        AST* e1 = parseExpr();
+        //might need changing
+        rel_op relop = parseOp();
+        AST* e2 = parseExpr();
+        return(ast_bin_cond(condt, e1, relop, e2));
+    }
 }
 
